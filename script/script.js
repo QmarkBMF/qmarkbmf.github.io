848b4073338a296b1d300be02d889426c248221d
QmarkBMF <qmarkbmf@yahoo.com>
2017-12-30 16:05:11 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,11 +2,18 @@ var remainingTime;
 
 function set_time(t) {
   if (t){
-    var days = Math.floor(remainingTime / 86400);
-    var hours = Math.floor((remainingTime % 86400) / 3600)
-    var minutes = Math.floor((remainingTime % 3600) / 60)
-    var seconds = Math.floor(remainingTime % 60)
-    document.getElementById("test").innerText = days + " д.  " + hours + " ч.  " + minutes + " мин.  ";
+    if (remainingTime != 0){
+      var days = Math.floor(remainingTime / 86400);
+      var hours = Math.floor((remainingTime % 86400) / 3600)
+      var minutes = Math.floor((remainingTime % 3600) / 60)
+      var seconds = Math.floor(remainingTime % 60)
+      document.getElementById("title").innerText = "Надя вернется через;
+      document.getElementById("test").innerText = days + " д.  " + hours + " ч.  " + minutes + " мин.  ";
+    }
+    else {
+      document.getElementById("title").innerText = " ";
+      document.getElementById("test").innerText = "Надя вернулась!";
+    }
   }// + seconds + "c. "
 }
 
